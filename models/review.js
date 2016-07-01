501f5640ede097c55598c122b81b3082c9793675
Michael Malleske <mmalleske@gmail.com>
2016-07-01 16:28:15 -0500

    partials added

--- models/review.js
+++ models/review.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var reviewSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  review: { type: String, required: true }
+  review: { type: String, required: true },
   image: { type: String}
 });
 
