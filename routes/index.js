bd358c0b08700e2d4b14aed780ac6b79340ea7ee
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-07-01 12:52:27 -0500

    Create skeletons of index.js routes and require models

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var SwimmingHole = require('../models/swimming_hole');
+var Review = require('../models/review');
 
-/* GET home page. */
+// PULL UP INDEX
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// PULL UP PAGE FOR CREATING A NEW REVIEW ON A SPECIFIC SWIMMING HOLE
+router.get('/:id/new', function(req, res, next) {
+
+});
+
+
+// PULL UP PAGE OF SPECIFIC SWIMMING HOLE
+router.get('/:id', function(req, res, next) {
+
+});
+
+
+// PULL UP PAGE FOR EDITING A REVIEW OF SPECIFIC SWIMMING HOLE
+router.get('/:id/edit', function(req, res, next) {
+
+});
+
+
+// CREATE A NEW REVIEW AND SAVE TO DATABASE
+router.post('/:id/new', function(req, res, next) {
+
+});
+
+
+// UPDATE A REVIEW AND SAVE TO DATABASE
+router.patch('/:id', function(req, res, next) {
+
+});
+
+
+// DELETE A REVIEW AND SAVE TO DATABASE
+router.delete('/:id', function(req, res, next) {
+
+});
+
 
 module.exports = router;
