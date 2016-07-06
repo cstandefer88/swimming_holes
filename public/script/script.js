7dde8d3fa4fde1015d2d9b0c3d49fd12babc885d
Christian Mansour <c.mansour@miami.edu>
2016-07-06 18:51:43 -0500

    finished all of the css for the swimming hole pages

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-  $('.fa-chevron-down').click(function () {
+  $('.arrow').click(function () {
     $('html,body').animate({
         scrollTop: $(document).height()/4.7,
+    }, 1000);
+  });
+  $('.reviewArrow').click(function () {
+    $('html,body').animate({
+        scrollTop: $(document).height()/2,
     }, 1000);
   });
 });
