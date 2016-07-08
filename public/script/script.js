da785caaaa0e5998a063364dc4d799ad286d3feb
Christian Mansour <c.mansour@miami.edu>
2016-07-07 19:08:30 -0500

    applied css to the edit page

--- public/script/script.js
+++ public/script/script.js
@@ -4,9 +4,4 @@ $(document).ready(function(){
         scrollTop: $(document).height()/4.4,
     }, 1000);
   });
-  $('.reviewArrow').click(function () {
-    $('html,body').animate({
-        scrollTop: $(document).height()/1.5,
-    }, 1000);
-  });
 });
